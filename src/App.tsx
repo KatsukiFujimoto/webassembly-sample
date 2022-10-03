29199fe1e16fa6f830fb12abf15e343dceea5e3e
KatsukiFujimoto <kf1996cnbl@gmail.com>
2022-10-03 09:38:14 +0900

    WebAssempblyを使ったフロントのサンプルコードを追加した

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, useState } from "react";
+import init, { sum } from "./lib/wasm";
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<div>Loading</div>}>
+      <AppContainer />
+    </Suspense>
   );
 }
+
+const AppContainer = (): JSX.Element => {
+  const [num, setNum] = useState(0);
+
+  init();
+  return (
+    <>
+      <input
+        onChange={(e) => {
+          const inputNum = Number(e.target.value);
+          !isNaN(inputNum) && setNum(sum(inputNum));
+        }}
+      />
+      <p>{num}</p>
+    </>
+  );
+};
 
 export default App;
